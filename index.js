960641b5af829b588aca0b1986e5d461a3b81ebe
NicoletaGheorghe <g.nicole261193@gmail.com>
2025-04-25 10:01:21 +0100

    add functions and images

--- index.js
+++ index.js
@@ -111,9 +111,9 @@ class Animal {
     }
 }
 const StartField = new Field("Start");
-StartField.description = "Welcome to <strong>Animal Rescue Adventure!</strong> Explore the magical fields using directions like 'north', 'south', 'east' or 'west'. " + 
+StartField.description = "Welcome to <strong>Animal Rescue Adventure!</strong> Explore the magical fields using directions like <em>'north'</em>, <em>'south'</em>, <em>'east'</em> or <em>'west'</em>. " + 
 "<strong>Look for animals</strong> in need and help them. <strong>Collect hearts</strong> along the way to show your kindness.\n" +
-"Type 'start' to begin your journey into the Flower Meadow.";
+"Type <em>'start'</em> to begin your journey into the Flower Meadow.";
 StartField.backgroundImage = "startImg.png";
 
 const FlowerMeadow = new Field ("Flower Meadow");
@@ -121,7 +121,7 @@ FlowerMeadow.description = "Butterflies flutter around";
 FlowerMeadow.backgroundImage = "kitten.png";
 FlowerMeadow.hasHearts = true;
 const kitten = new Animal("kitten");
-kitten.description = "stuck in flowers, can you rescue it? To the east there's a Hollow Tree, and from south, you can hear Chiriping Birds. Where would you like to go?"; 
+kitten.description = "stuck in flowers, can you rescue it? To the <em>east</em> there's a Hollow Tree, and from <em>south</em>, you can hear Chiriping Birds. Where would you like to go?"; 
 FlowerMeadow.animal = kitten;
 
 const ChirpingBirdsMeadow = new Field ("Chirping Birds Meadow");
@@ -129,7 +129,7 @@ ChirpingBirdsMeadow.description = "Dozens of colourful birds flutter from branch
 ChirpingBirdsMeadow.backgroundImage = "bird.png";
 ChirpingBirdsMeadow.hasHearts = false;
 const bird = new Animal("bird");
-bird.description = "with a hurt wing. It needs your help. To the north, you can return to the Flower Meadow, and to the east, you can see the Bunny Burrow. Where would you like to go next?"
+bird.description = "with a hurt wing. It needs your help. To the <em>north</em>, you can return to the Flower Meadow, and to the <em>east</em>, you can see the Bunny Burrow. Where would you like to go next?"
 ChirpingBirdsMeadow.animal = bird;
 
 const HollowTree = new Field ("Hollow Tree");
@@ -137,7 +137,7 @@ HollowTree.description = "A massive tree towers before you, its wide trunk is ho
 HollowTree.backgroundImage = "squirrel.png";
 HollowTree.hasHearts = true;
 const squirrel = new Animal("squirrel");
-squirrel.description = "stuck between its twisted branches. She looks at you with hopeful eyes. Can you help her? To the south, you can see the Bunny Burrow, but to the west, you can walk into the Flower Meadow. What would you like to explore?";
+squirrel.description = "stuck between its twisted branches. She looks at you with hopeful eyes. Can you help her? To the <em>south</em>, you can see the Bunny Burrow, but to the <em>west</em>, you can walk into the Flower Meadow. What would you like to explore?";
 HollowTree.animal = squirrel;
 
 const BunnyBurrow = new Field("Bunny Burrow");
@@ -145,7 +145,7 @@ BunnyBurrow.description = "Soft clover covers the field like a green blanket. At
 BunnyBurrow.backgroundImage = "rabbit.png";
 BunnyBurrow.hasHearts = true;
 const bunny = new Animal("bunny");
-bunny.description = "stuck. She looks scared. Would you help her? From the west, you can hear Chirping Birds, and to the north you see the Hollow Tree. Where would you go?"
+bunny.description = "stuck. She looks scared. Would you help her? From the <em>west</em>, you can hear Chirping Birds, and to the <em>north</em> you see the Hollow Tree. Where would you go?"
 BunnyBurrow.animal = bunny;
 
 StartField.linkField("start", FlowerMeadow);
@@ -157,3 +157,64 @@ BunnyBurrow.linkField("west", ChirpingBirdsMeadow);
 BunnyBurrow.linkField("north", HollowTree);
 HollowTree.linkField("south", BunnyBurrow);
 HollowTree.linkField("west", FlowerMeadow);
+
+let currentField = StartField;
+let heartsInventory = 0;
+let rescuedAnimals = 0;
+const output = document.getElementById("gameDescription");
+const result = document.getElementById("result") 
+
+function displayField(field){
+    const fieldText = document.getElementById("gameDescription");
+    const describeField = document.getElementById("describeField");
+
+    const fieldDescription = field.describe();
+    describeField.innerHTML = `
+    <p>${fieldDescription}</p>`;
+
+    fieldText.innerHTML = `
+    <p>You collected: ${heartsInventory} hearts.
+    <p>You rescued: ${rescuedAnimals} animals.`;
+
+    const bkGameSpace = document.getElementById("gameSpace");
+    bkGameSpace.style.backgroundImage =`url(${currentField.backgroundImage})`;
+    bkGameSpace.style.backgroundSize = "cover";
+    bkGameSpace.style.opacity = "0.8";
+}
+function checkWin(){
+    const inputBox = document.getElementById("userInput");
+    if(heartsInventory === 3 && rescuedAnimals === 4){
+        result.innerHTML += `<p>Congratulations!You have saved all the animals and collected all the hearts.<strong>You win!</strong></p>`
+        inputBox.disabled = true;
+    }
+    return output;
+}
+
+document.addEventListener("keydown", function(e){
+    if(e.key === "Enter"){
+       const inputBox = document.getElementById("userInput");
+       const command = inputBox.value.trim().toLowerCase();
+       const directions = ["north", "south", "east", "west", "start"];
+       
+         if(command === "collect heart" && currentField.hasHearts){
+            heartsInventory ++;
+            currentField.hasHearts = false;
+            displayField(currentField);
+        }else if(directions.includes(command)){
+            currentField =currentField.move(command);
+            displayField(currentField);
+        }else if(command === "rescue animal" && currentField.animal && !currentField.animal._isRescued){
+            output.innerHTML += currentField.animal.rescue();
+            rescuedAnimals ++;
+              displayField(currentField);
+        }
+        checkWin();
+        inputBox.value = "";
+    }
+});
+function startGame(){
+    currentField = StartField;
+    displayField(currentField);
+}
+
+window.onload = startGame;
